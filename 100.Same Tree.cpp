aecc9f04a52d6d25f18e7a2029999501d9bc9681
TristaWWP <31362022+TristaWWP@users.noreply.github.com>
2019-03-26 22:06:49 +0800

    递归判断两个二叉树是否相等，从根节点开始，然后依次递归两个左孩子和两个右孩子

---
+++ 100.Same Tree.cpp
@@ -0,0 +1,22 @@
+//给出两个二叉树，判断是否相同
+
+//相同就是节点的值和位置都相同，递归是最简单的
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if ((p && !q) || (!p && q) || (p->val != q->val)) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        
+    }
+};
