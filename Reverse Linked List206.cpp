8536713c86cd635551840c4199aa442563f64b00
TristaWWP <31362022+TristaWWP@users.noreply.github.com>
2019-04-16 20:52:06 +0800

    就地箭头转过来,箭头转过来，类似交换两数的

---
+++ Reverse Linked List206.cpp
@@ -0,0 +1,25 @@
+//循环算法,迭代，思路就是就地箭头转过来
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+      if (head == NULL) return head;//注意一定要考虑指针是否为空，不然就是思路不周祥
+        ListNode* pre = NULL;
+        ListNode* cur = head;
+        while (cur) {
+            ListNode* p = cur->next;//cur跟p依次后移
+            cur->next = pre;//箭头转过来，类似交换两数的
+            pre = cur;
+            cur = p;
+        }
+        return pre;
+    }
+};
+
+//递归
+ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+        ListNode* p = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return p;
